f65b34a32e04c90300322a30cffce5bcfcbb049d
mdzura <mdzura@gmail.com>
2020-02-07 20:14:00 -0500

    added router and switch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import BodyContent from './components/BodyContent.js';
+//import BodyContent from './components/BodyContent.js';
 import NavBar from './components/header/NavBar.js';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./components/pages/Home.js";
+import VehicleList from "./components/pages/VehicleList.js";
+import About from "./components/pages/About.js";
 
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <BodyContent />
+      
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/vehicle-list" component={VehicleList} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </Router>
+      
     </div>
     
   )
