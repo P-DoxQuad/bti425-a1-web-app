686b58c13bfb527064a0f18810c9d266ee18cdb3
mdzura <mdzura@gmail.com>
2020-02-07 23:40:02 -0500

    removed import for routing

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavButton from '../NavButton'
 
 class NavBar extends Component {
