0d9eeb919aa20a653b9c434ad90e32072f5d91e7
mdzura <mdzura@gmail.com>
2020-02-07 21:24:49 -0500

    updated home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,8 +5,9 @@ function Home() {
         <div class="container-fluid text-center">
             <div class="row content">
                 <div class="col-sm-8 text-left">
-                    <h1>Welcome</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <h1>BTI 425</h1>
+                    <h3>Assignment #1</h3>
+                    <p>Use this App to view a list of vehicles, as well as Add, or Delete a vehicle from the records.</p>
                 </div>
             </div>
         </div>
