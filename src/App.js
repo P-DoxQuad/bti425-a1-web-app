2cf779f0905e97500cc2159a9bc4cd0a59744d46
mdzura <mdzura@gmail.com>
2020-02-09 02:03:49 -0500

    added route for details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-//import BodyContent from './components/BodyContent.js';
 import NavBar from './components/header/NavBar.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from "./components/pages/Home.js";
 import VehicleList from "./components/pages/VehicleList.js";
+import VehicleDetail from "./components/pages/VehicleDetail.js";
 import AddVehicle from "./components/pages/AddVehicle.js";
 import DelVehicle from "./components/pages/DelVehicle.js";
 import About from "./components/pages/About.js";
@@ -21,6 +21,7 @@ function App() {
           <Route exact path="/" render={() => (<Home />)} />
           <Route exact path="/vehicles" render={() => (<VehicleList />)} />
           <Route exact path="/vehicles/:id" render={(props) => (<VehicleList id={props.match.params.id}/>)} />
+          <Route exact path="/vehicle/detail/:id" render={(props) => (<VehicleDetail id={props.match.params.id}/>)} />
           <Route exact path="/vehicle/add/" render={() => (<AddVehicle />)} />
           <Route exact path="/vehicle/delete/:id" render={(props) => (<DelVehicle id={props.match.params.id}/>)} />
           <Route exact path="/about" render={() => (<About />)} />
