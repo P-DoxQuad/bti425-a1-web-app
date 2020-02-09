ed1b0273d9df3946de6577ae214726aa37b88915
mdzura <mdzura@gmail.com>
2020-02-08 23:44:03 -0500

    fixed fetch. it works now!

--- src/components/pages/VehicleList.js
+++ src/components/pages/VehicleList.js
@@ -6,13 +6,12 @@ class VehicleList extends Component {
 
   state = { vehicles: [] };
 
-  url = "https://bti425-a1-web-api.herokuapp.com/api/vehicles/";
-
   componentDidMount() {
+    const url = "https://bti425-a1-web-api.herokuapp.com/api/vehicles/";
 
     // Get all
-    fetch(this.url)
-      .then(function(response) {
+    fetch(url)
+      .then(response => {
         // Optional...
         this.setState({ httpStatusCode: response.status, httpStatusOk: response.ok });
         if (response.ok) {
@@ -26,12 +25,12 @@ class VehicleList extends Component {
           throw Error(`HTTP ${response.status}, ${response.statusText}`);
         }
       })
-      .then(function(responseData) {
+      .then(responseData => {
         // "responseData" is an object; here, we're interested in its "data" property
         // Study the shape of the data in the reqres.in service
-        this.setState({ vehicles: responseData.data });
+        this.setState({ vehicles: responseData });
         // Optional...
-        console.log(responseData.data);
+        //console.log(responseData.data);
       })
       .catch(error => {
         // Handles an error thrown above, as well as network general errors
@@ -45,8 +44,8 @@ class VehicleList extends Component {
 
     return (
       <div>
-        <h4>List of Vehicles, from the reqres.in service</h4>
-        <p><Link className='btn btn-default' to='/users/create'>Add a New Vehicle</Link></p>
+        <h4>List of Vehicles, from custom WEB API service</h4>
+        <p><Link className='btn btn-default' to='/add-vehicle'>Add a Vehicle</Link></p>
         <table className='table table-striped'>
           <TableHeader />
           <TableBody vehicles={this.state.vehicles} />
@@ -55,6 +54,8 @@ class VehicleList extends Component {
     );
   }
 }
+
+export default VehicleList;
 
 // ############################################################
 // Most of the following was copied from the react-tania-updated code example
@@ -69,7 +70,9 @@ const TableHeader = () => {
         <th>Photo</th>
         <th>Make</th>
         <th>Model</th>
+        <th>Colour</th>
         <th>Year</th>
+        <th>VIN</th>
         <th>MSRP</th>
       </tr>
     </thead>
@@ -81,14 +84,11 @@ const TableHeader = () => {
 const TableBody = (props) => {
 
   // Using the array of objects, create a new array of React elements
-  if (props.vehicles) {
-    var rows = props.vehicles.map(function (vehicle, index) {
-      return (
-        <TableRow vehicle={vehicle} key={index} />
-      );
-    });
-  }
-  
+  let rows = props.vehicles.map((vehicle, index) => {
+    return (
+      <TableRow vehicle={vehicle} key={index} />
+    );
+  });
 
   return <tbody>{rows}</tbody>
 }
@@ -101,23 +101,23 @@ const TableRow = props => {
   const v = props.vehicle;
 
   // Alternative declaration syntax...
-  //const { v } = this.props;
+  //const { u } = this.props;
 
   // Render the row
   return (
     <tr>
       <td>{v.id}</td>
-      <td><img src={v.avatar} alt='' className='imgInTable' /></td>
+      <td><img src={v.photo} alt='' className='imgInTable' /></td>
       <td>{v.make}</td>
       <td>{v.model}</td>
       <td>{v.colour}</td>
-      <td>{v.model}</td>
+      <td>{v.year}</td>
+      <td>{v.vin}</td>
       <td>{v.msrp}</td>
       <td><Link className='btn btn-default' to={`/vehicles/detail/${v.id}`}>Details</Link>&nbsp;&nbsp;
-          <Link className='btn btn-warning' to={`/vehicles/edit/${v.id}`}>Edit</Link>&nbsp;&nbsp;
-          <Link className='btn btn-danger' to={`/vehicles/delete/${v.id}`}>Delete</Link></td>
+            <Link className='btn btn-warning' to={`/vehicles/edit/${v.id}`}>Edit</Link>&nbsp;&nbsp;
+            <Link className='btn btn-danger' to={`/vehicle/delete/${v.id}`}>Delete</Link></td>
     </tr>
   );
 }
 
-export default VehicleList
