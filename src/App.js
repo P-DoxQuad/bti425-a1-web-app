8e7b87eadf04ac3d3b2392a83e0809ebf695ed95
mdzura <mdzura@gmail.com>
2020-02-08 23:41:06 -0500

    more route handling

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from "./components/pages/Home.js";
 import VehicleList from "./components/pages/VehicleList.js";
 import AddVehicle from "./components/pages/AddVehicle.js";
+import DelVehicle from "./components/pages/DelVehicle.js";
 import About from "./components/pages/About.js";
+
+//import UserList from "./components/pages/FetchTest.js";
+
 
 
 function App() {
   return (
     <div>
-      
       <Router>
         <NavBar />
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/vehicle-list" component={VehicleList} />
-          <Route exact path="/add-vehicle" component={AddVehicle} />
-          <Route exact path="/about" component={About} />
+          <Route exact path="/" render={() => (<Home />)} />
+          <Route exact path="/vehicles" render={() => (<VehicleList />)} />
+          <Route exact path="/vehicles/:id" render={(props) => (<VehicleList id={props.match.params.id}/>)} />
+          <Route exact path="/vehicle/add/" render={() => (<AddVehicle />)} />
+          <Route exact path="/vehicle/delete/:id" render={(props) => (<DelVehicle id={props.match.params.id}/>)} />
+          <Route exact path="/about" render={() => (<About />)} />
         </Switch>
       </Router>
-      
     </div>
     
   )
