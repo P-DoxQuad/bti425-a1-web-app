7fdda1cf8fb32292f2fca45a8dc015477b223716
mdzura <mdzura@gmail.com>
2020-02-13 02:00:57 -0500

    added reference and simple validation

--- src/components/pages/AddVehicle.js
+++ src/components/pages/AddVehicle.js
@@ -11,6 +11,18 @@ class AddVehicle extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.inputMake = React.createRef();
+        this.inputModel = React.createRef();
+        this.inputColour = React.createRef();
+        this.inputYear = React.createRef();
+        this.inputVIN = React.createRef();
+        this.inputMSRP = React.createRef();
+        this.inputPhoto = React.createRef();
+        this.inputDesc = React.createRef();
+        this.inputPurchaseDate = React.createRef();
+        this.inputPurchaseName = React.createRef();
+        this.inputPurchaseEmail = React.createRef();
+        this.inputPricePaid = React.createRef();
     }
 
     state = {
@@ -62,56 +74,80 @@ class AddVehicle extends Component {
         const url = "https://bti425-a1-web-api.herokuapp.com/api/vehicle";
         //const url = "http://localhost:8080/api/vehicle";
 
-        const newVehicle = {
-            //'id': 202,
-            'make': this.state.make,
-            'model': this.state.model,
-            'colour': this.state.colour,
-            'year': this.state.year,
-            'vin': this.state.vin,
-            'msrp': this.state.msrp,
-            'photo': this.state.photo,
-            'description': this.state.description,
-            'purchaseDate': this.state.purchaseDate,
-            'purchaserName': this.state.purchaserName,
-            'purchaserEmail': this.state.purchaserEmail,
-            'pricePaid': this.state.pricePaid
-        };
+        
+        var setError = 0;
 
-        fetch(url, {
-            method: 'POST',
-            mode: 'cors',
-            headers: { 
-                //"Content-Type": 'application/x-www-form-urlencoded'
-                "Content-Type": 'application/json' 
-            },
-            body: JSON.stringify(newVehicle)
-        })
-            .then(response => {
-                console.log("Posting Vehicle from Form");
-                if (response.ok) {
-                    // Parse the response body as JSON
-                    return response.json();
-                } else if (response.status >= 400 && response.status < 500) {
-                    // Error caused by the requestor
-                    throw Error(`HTTP ${response.status}, ${response.statusText}`);
-                } else {
-                    // Some other situation
-                    throw Error(`HTTP ${response.status}, ${response.statusText}`);
-                }
+        if (!this.inputMake.current.value) {
+            alert("'Make' is empty!");
+        } else if (!this.inputModel.current.value) {
+            alert("'Model' is empty!");
+        } else if (!this.inputColour.current.value) {
+            alert("'Colour' is empty!");
+        } else if (isNaN(this.inputYear.current.value)) {
+            alert("'Year' must be a number, like - 1999");
+        } else if (!this.inputYear.current.value) {
+            alert("'Year' is empty!");
+        } else if (!this.inputVIN.current.value) {
+            alert("'VIN Number' is empty!");
+        } else if (!this.inputMSRP.current.value) {
+            alert("'MSRP' is empty!");
+        } else if (isNaN(this.inputMSRP.current.value)) {
+            alert("Must be a number, no commas, like - 2000");
+        } else if (!this.inputPhoto.current.value) {
+            alert("'Photo' is empty!");
+        } else {
+
+            const newVehicle = {
+                //'id': 202,
+                'make': this.state.make,
+                'model': this.state.model,
+                'colour': this.state.colour,
+                'year': this.state.year,
+                'vin': this.state.vin,
+                'msrp': this.state.msrp,
+                'photo': this.state.photo,
+                'description': this.state.description,
+                'purchaseDate': this.state.purchaseDate,
+                'purchaserName': this.state.purchaserName,
+                'purchaserEmail': this.state.purchaserEmail,
+                'pricePaid': this.state.pricePaid
+            };
+
+            fetch(url, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    //"Content-Type": 'application/x-www-form-urlencoded'
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify(newVehicle)
             })
-            .then(responseData => {
-                // "responseData" is an object
-                // Study the shape of the data in the reqres.in service
-                // Optional...
-                console.log(responseData);
-                // The identifier "id" can be used to redirect
-                this.props.history.push(`/vehicles`);
-            })
-            .catch(error => {
-                // Handles an error thrown above, as well as network general errors
-                console.log(error.message)
-            });
+                .then(response => {
+                    console.log("Posting Vehicle from Form");
+                    if (response.ok) {
+                        // Parse the response body as JSON
+                        return response.json();
+                    } else if (response.status >= 400 && response.status < 500) {
+                        // Error caused by the requestor
+                        throw Error(`HTTP ${response.status}, ${response.statusText}`);
+                    } else {
+                        // Some other situation
+                        throw Error(`HTTP ${response.status}, ${response.statusText}`);
+                    }
+                })
+                .then(responseData => {
+                    // "responseData" is an object
+                    // Study the shape of the data in the reqres.in service
+                    // Optional...
+                    console.log(responseData);
+                    // The identifier "id" can be used to redirect
+                    this.props.history.push(`/vehicles`);
+                })
+                .catch(error => {
+                    // Handles an error thrown above, as well as network general errors
+                    console.log(error.message)
+                });
+        }
 
     }
 
@@ -138,14 +174,14 @@ class AddVehicle extends Component {
                                     <div className="col-md-6">
                                         <div className="form-group">
                                             <label htmlFor="make">Make:</label>
-                                            <input className="form-control" id="make" name="make" ref={(i) => { this.input = i; }} 
+                                            <input className="form-control" id="make" name="make" ref={this.inputMake} 
                                                    onChange={this.handleChange} type="text" />
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <div className="form-group">
                                             <label htmlFor="model">Model:</label>
-                                            <input className="form-control" id="model" name="model" ref={(i) => { this.input = i; }}
+                                            <input className="form-control" id="model" name="model" ref={this.inputModel}
                                                    onChange={this.handleChange} type="text" />
                                         </div>
                                     </div>
@@ -154,14 +190,14 @@ class AddVehicle extends Component {
                                     <div className="col-md-6">
                                         <div className="form-group">
                                             <label htmlFor="colour">Colour:</label>
-                                            <input className="form-control" id="colour" name="colour" ref={(i) => { this.input = i; }}
+                                            <input className="form-control" id="colour" name="colour" ref={this.inputColour}
                                                    onChange={this.handleChange} type="text" />
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <div className="form-group">
                                             <label htmlFor="year">Year:</label>
-                                            <input className="form-control" id="year" name="year" ref={(i) => { this.input = i; }}
+                                            <input className="form-control" id="year" name="year" ref={this.inputYear}
                                                    onChange={this.handleChange} type="text" />
                                         </div>
                                     </div>
@@ -170,22 +206,26 @@ class AddVehicle extends Component {
                                     <div className="col-md-6">
                                         <div className="form-group">
                                             <label htmlFor="vin">Vin Number:</label>
-                                            <input className="form-control" id="vin" name="vin" onChange={this.handleChange} type="text" />
+                                            <input className="form-control" id="vin" name="vin" ref={this.inputVIN}
+                                                   onChange={this.handleChange} type="text" />
                                         </div>
                                     </div>
                                     <div className="col-md-6">
                                         <label htmlFor="msrp">MSRP:</label>
-                                        <input className="form-control" id="msrp" name="msrp" onChange={this.handleChange} type="text" />
+                                        <input className="form-control" id="msrp" name="msrp" ref={this.inputMSRP}
+                                               onChange={this.handleChange} type="text" />
                                     </div>
                                     <div className="col-md-12">
                                         <div className="form-group">
                                             <label htmlFor="description">Description:</label>
-                                            <textarea className="form-control" id="description" name="description" onChange={this.handleChange} type="text" />
+                                            <textarea className="form-control" id="description" name="description" ref={this.inputDesc}
+                                                      onChange={this.handleChange} type="text" />
                                         </div>
                                     </div>
                                     <div className="col-md-12">
                                         <label htmlFor="photo">Photo:</label>
-                                        <input className="form-control" id="photo" name="photo" onChange={this.handleChange} type="text" />
+                                        <input className="form-control" id="photo" name="photo" ref={this.inputPhoto}
+                                               onChange={this.handleChange} type="text" />
                                     </div>
                                 </div>
                             </fieldset>
