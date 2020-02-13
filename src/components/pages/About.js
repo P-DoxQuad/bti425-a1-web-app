2615b4a0b519365d1e9742598034c4358f7148b7
mdzura <mdzura@gmail.com>
2020-02-13 02:00:14 -0500

    added about me - WIP

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,7 @@
 import React from 'react'
 
 function About() {
-    const dateTime = (
-        <React.Fragment>
-            Today is {new Date().toLocaleDateString}, {new Date().getMonth.toString};
-        </React.Fragment>
-        
-    )
+
 
     return (
         <React.Fragment>
@@ -15,8 +10,23 @@ function About() {
             </div>
 
             <div>
-                <p>Hello, my name is Michael</p>
-                <p>{dateTime}</p>
+                <table align="center">
+                    <tr>
+                        <td><img src="http://www.uniforlocal40.org/wp-content/uploads/2018/04/10389478_10153275748387802_8156825980015189853_n-e1526579355360.jpg" /></td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Hello, my name is Michael. I was born and raised in Toronto, Ontario. I am a 2016 CPD graduate from Seneca College, and currently enrolled in BSD.</p>
+
+                            <p>My passion for technology began at a very young age. I saw the digital world as being inclusive of everyone despite differences of abilities. With so much ingenuity and possibilities to explore, I took the initiative to learn as much as I could on my own with Hardware and Software since these areas could not be held back by my disability. 14 years and counting, I have developed a wealth of knowledge in various areas.</p>
+
+                            <p>In my spare time I like to code. It really isn’t a job. Some of the most satisfying moments are when you code something that actually works! I also enjoy reading Tech articles; always looking for cool things to play with that I can get a hold of. When I need more of a break from the world, you’ll either catch me on Netflix, Youtube, or playing a video game. My Top 5 All-Time Favourite PC Games are Half Life 2 (Entire Saga), Portal 2, Left 4 Dead, Command and Conquer Tiberium Wars, and Grand Theft Auto San Andreas.</p>
+
+                            <p>I like to eat chicken. If you need a project done, bring me a junior chicken sandwich and we'll talk... I'll accept a box of Kraft Dinner too as an alternative.”</p>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </React.Fragment>
     )
